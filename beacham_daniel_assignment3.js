4eedd70784f79e283d2d6c09717b11cc0a7edce2
Daniel Beacham <dkuwf15@gmail.com>
2012-12-04 20:22:34 -0500

    Creation of Employee Pattern and properties

--- beacham_daniel_assignment3.js
+++ beacham_daniel_assignment3.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+//Global Vars
+var companyName = "ACME Testing";
+
+var employee = (function(){
+
+    var isContractor = true;
+    var skillSet = ['PHP', 'MySQL', 'Javascript', 'jQuery'];
+    var salaryLevel = 5;
+    var jobDescription = 'Web Developer';
+    var company =  {
+        address: '4540 Main Street',
+        numEmployees: 500
+    }
+
+    return {
+        setNewEmployeename : function() {
+            this.name = name;
+        },
+        getEmployeeJobDescription: function() {
+            return jobDescription;
+        },
+        calculateSalary : function() {
+          var salaryMultiplier = 15000;
+
+          return salaryMultiplier * salaryLevel;
+        }
+
+    };
+
+})();
+
