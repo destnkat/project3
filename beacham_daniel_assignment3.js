c5d3341d9cb92dbf941f37732a6ac81f46ad51bf
Daniel Beacham <dkuwf15@gmail.com>
2012-12-05 00:02:59 -0500

    Created Employee Class and prototyped the methods

--- beacham_daniel_assignment3.js
+++ beacham_daniel_assignment3.js
@@ -1,31 +1,51 @@
 //Global Vars
-var companyName = "ACME Testing";
+var companyName = "ACME Testing",
+    specialSkills = ['Javascript', 'HTML5', 'CSS'];
+    bonusOffered = false;
+    lastEmployeeId = 4667;
 
-var employee = (function(){
-
-    var isContractor = true;
-    var skillSet = ['PHP', 'MySQL', 'Javascript', 'jQuery'];
-    var salaryLevel = 5;
-    var jobDescription = 'Web Developer';
-    var company =  {
-        address: '4540 Main Street',
-        numEmployees: 500
-    }
-
-    return {
-        setNewEmployeename : function() {
-            this.name = name;
+function Employee (empInfo) {
+    //Local Vars
+    var isContractor = empInfo.contractor,
+        skillSet = ['PHP', 'MySQL', 'Javascript', 'jQuery'],
+        employeeId = lastEmployeeId++,
+        jobDescription = empInfo.position,
+        company = {
+            name : companyName,
+            numEmployees:500
         },
-        getEmployeeJobDescription: function() {
-            return jobDescription;
-        },
-        calculateSalary : function() {
-          var salaryMultiplier = 15000;
+        startingSalary = 0;
 
-          return salaryMultiplier * salaryLevel;
+
+    Employee.prototype.setStartingSalary = function (salary) { //mutator
+        startingSalary = salary;
+    },
+
+    Employee.prototype.getTotalCompensation = function() {  //Accessor
+        return startingSalary;
+    },
+    Employee.prototype.calculateSalary = function () {
+        for (var i=0; i < skillSet.length; i++) {  //For Loop
+            if (specialSkills.indexOf(skillSet[i]) > -1) {  //Conditional
+                startingSalary += 2500;
+                if (isContractor && bonusOffered) {  //Nested Conditional
+                    startingSalary += 1000;
+                }
+            }
         }
+    }
+};
 
-    };
+window.onload = function(){
+    var employeeInfo =  {
+        name : 'Adam',
+        position: 'Web Developer',
+        contractor : 'true'
+    }
+    
+    var adam = new Employee(employeeInfo);
+        adam.setStartingSalary(75000);
+        adam.calculateSalary();
 
-})();
-
+    console.log(adam.getTotalCompensation());
+};
