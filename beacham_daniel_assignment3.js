801bb5582859328210dae74399c8f0223cf175d2
Daniel Beacham <dkuwf15@gmail.com>
2012-12-06 00:33:43 -0500

    Re-Arrange the Employee Object, Prototype the Methods and updating method calls

--- beacham_daniel_assignment3.js
+++ beacham_daniel_assignment3.js
@@ -1,37 +1,97 @@
 //Global Vars
-var companyName = "ACME Testing",
-    specialSkills = ['Javascript', 'HTML5', 'CSS'];
-    lastEmployeeId = 4667;
+var lastEmployeeId = 4667;
+
+//JSON Data For Candidate Pool
+var candidatesJSON = {
+    candidates : [
+        {
+            "name": "Beast",
+            "position": "Web Developer",
+            "contractor": true,
+            "negotiationLevel" : 4,
+            "skills": [
+                "PHP",
+                "MySQL"
+            ]
+        },
+        {
+            "name": "Belle",
+            "position": "Web Developer",
+            "contractor": false,
+            "negotiationLevel" : 4,
+            "skills": [
+                "PHP",
+                "MySQL",
+                "Javascript",
+                "jQuery",
+                "CSS",
+                "JAVA"
+            ]
+        },
+        {
+            "name": "Meeko",
+            "position": "Web Developer",
+            "contractor": true,
+            "negotiationLevel" : 1,
+            "skills": [
+                "PHP",
+                "MySQL",
+                "Javascript",
+                "jQuery",
+                "Oracle"
+            ]
+        }
+    ]
+}
 
 function Employee (empInfo) {
     //Local Vars
-    var isContractor = empInfo.contractor,
-        bonusOffered = empInfo.bonusEligible,
-        skillSet = empInfo.skills,
-        jobDescription = empInfo.position,
-        employeeId,
-        startingSalary;
+    this.isContractor = empInfo.contractor,
+    this.skillSet = empInfo.skills,
+    this.jobDescription = empInfo.position,
+    this.employeeName = empInfo.name,
+    this.bonusOffered = isBonusEligible(empInfo.negotiationLevel),
+    this.employeeId,
+    this.startingSalary;
 
-    Employee.prototype.setStartingSalary = function (salary) { //mutator
-        startingSalary = salary;
-    },
 
-    Employee.prototype.getTotalCompensation = function() {  //Accessor
-        return startingSalary;
-    },
-    Employee.prototype.calculateSalary = function () {
-        for (var i=0; i < skillSet.length; i++) {  //For Loop
-            if (specialSkills.indexOf(skillSet[i]) > -1) {  //Conditional
-                startingSalary += 2500;
-                if (isContractor && bonusOffered) {  //Nested Conditional
-                    startingSalary += 1000;
-                }
-            }
-        }
+    function isBonusEligible(negLevel) {
+        return negLevel === 1 ? true : false;  //Return Boolean
+    }
+
+    function setEmployeeId() {
+       this.employeeId = lastEmployeeId++;
     }
 };
 
-function startTheJobHunt(candidates) {
+Employee.prototype.setStartingSalary = function(salary) { //mutator
+        this.startingSalary = salary;
+};
+
+Employee.prototype.getTotalCompensation = function() {  //Accessor
+        console.log(this.employeeName + ' will start with a salary of ' + this.startingSalary);
+        return this.startingSalary;  //Return Number
+};
+
+Employee.prototype.getJobDescription = function() {
+        console.log(this.employeeName + ' is hired as a ' + this.jobDescription);
+        return this.jobDescription;  //Return String
+};
+Employee.prototype.calculateSalary = function (specialSkills) {
+        var skills = this.skillSet;
+        for (var i=0; i < skills.length; i++) {  //For Loop
+            if (specialSkills.indexOf(skills[i]) > -1) {  //Conditional
+                this.startingSalary += 2500;
+                if (this.isContractor && this.bonusOffered) {  //Nested Conditional
+                    this.startingSalary += 1000;
+                }
+            }
+        }
+};
+
+function startTheJobHunt(obj) {
+    var candidates = obj.candidates;
+
     for(var i = 0; i < candidates.length; i++) {
         var interviewProcess = 'begin';
 
@@ -40,34 +100,24 @@ function startTheJobHunt(candidates) {
             interviewProcess = 'complete';
         }
         //Not coding the Entire Interview Process, so I took a shortcut by hard coding a winner :)
-        if (candidates[i].toLowerCase() === 'meeko') {
-
-            console.log(candidates[i] + ' Is who we want to hire. He is Awesome!')
-            return {
-                "name": candidates[i],
-                "position": "Web Developer",
-                "contractor": true,
-                "bonusEligible": false,
-                "skills": [
-                    "PHP",
-                    "MySQL",
-                    "Javascript",
-                    "jQuery"
-                ]
-            }
+        if (candidates[i].name.toLowerCase() === 'meeko') {
+            console.log(candidates[i].name + ' Is who we want to hire. He is Awesome!')
+            return candidates[i];  // Returns Object
         } else {
-            console.log(candidates[i] + ' Is Not Hired');
+            console.log(candidates[i].name + ' Is Not Hired');
         }
     }
 }
 
 window.onload = function(){
-    var candidates = ['Aurora', 'Beast', 'Belle', 'Naveen', 'Meeko'];
-    var winningCandidate = startTheJobHunt(candidates);
+    var specialSkills = ['Javascript', 'HTML5', 'CSS'];
+    var winningCandidate = startTheJobHunt(candidatesJSON);
     var hiredEmployee = new Employee(winningCandidate);
 
     hiredEmployee.setStartingSalary(75000);
-    hiredEmployee.calculateSalary();
+    hiredEmployee.calculateSalary(specialSkills);
 
-    console.log('Total Compensation: ' + hiredEmployee.getTotalCompensation());
+    //Retrieve and console log
+    hiredEmployee.getJobDescription();
+    hiredEmployee.getTotalCompensation();
 };
